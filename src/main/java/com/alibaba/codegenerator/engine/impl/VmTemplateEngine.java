c7c27ea65cadffe3ea9d6d748251f56866f04ad6
善马 <kebin.ckb@alibaba-inc.com>
2016-06-16 16:54:54 +0800

    update

--- src/main/java/com/alibaba/codegenerator/engine/impl/VmTemplateEngine.java
+++ src/main/java/com/alibaba/codegenerator/engine/impl/VmTemplateEngine.java
@@ -11,8 +11,6 @@ import org.apache.commons.io.output.FileWriterWithEncoding;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
